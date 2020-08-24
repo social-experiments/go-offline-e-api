af6e26ce7a1bb109749a335364b60d4456338efd
zencondchaskar <dattatraya.chaskar@zconsolutions.com>
2020-08-24 16:33:14 +0530

    azure blob SAS token generation function

---
+++ Models/BlobStorageRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace goOfflineE.Models
+{
+    public class BlobStorageRequest
+    {
+        public string StorageUri { get; set; }
+        public string StorageAccessToken { get; set; }
+    }
+}
---
+++ Services/IAzureBlobService.cs
@@ -0,0 +1,13 @@
+﻿using goOfflineE.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goOfflineE.Services
+{
+    public interface IAzureBlobService
+    {
+        Task<BlobStorageRequest> GetSasUri(string containerName);
+    }
+}
